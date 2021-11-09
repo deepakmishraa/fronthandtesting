ad4bab485566b19022d3c6e2c5eb2b47e7767eb8
Deepak Mishra <mishradeepak0125@gmail.com>
2021-11-09 05:42:06 +0530

    api is

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EnvService {
+  // API_URL = 'https://nodedemoappl.herokuapp.com/api'
   API_URL = 'https://nodedemoappl.herokuapp.com/api'
   constructor() { }
 }
